f4b23050a05d02ff45605c36b58b1a7e49116106
loxt <emannuel.matos@begrowth.co>
2021-12-07 23:58:05 -0400

    test: credit card expiration month

--- golang/domain/entities/credit_card_test.go
+++ golang/domain/entities/credit_card_test.go
@@ -1,1 +1,18 @@
 package entities
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreditCardNumber(t *testing.T) {
+	_, err := NewCreditCard("40000000000000000", "Emannuel Matos", 12, 2024, 123)
+	assert.Equal(t, "invalid credit card number", err.Error())
+
+	_, err = NewCreditCard("4193523830170205", "Emannuel Matos", 12, 2024, 123)
+	assert.Nil(t, err)
+}
+
+func TestCreditCardExpirationMonth(t *testing.T) {
+
+}
